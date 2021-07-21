a06bec17f29e4a01f0113b005e2d8de04d3caa45
Alec Dutcher <77470743+aidutcher@users.noreply.github.com>
2021-07-21 06:29:52 -0500

    Update gen_inv_data.py

--- Generate and Update Data/gen_inv_data.py
+++ Generate and Update Data/gen_inv_data.py
@@ -4,20 +4,26 @@ from numpy.random import randn
 import random, string
 from datetime import date
 
+# Change the number in range() to set the number of rows
+row_count = range(1000)
+
+# Generate a three letter code to identify the vendor the item was purchased from
 def gen_vend_code():
-    vend_list =[]
+    vend_code_list =[]
     for i in range(10):
         y = ''.join(random.choices(string.ascii_uppercase, k=3))
-        vend_list.append(y)
-    return vend_list
+        vend_code_list.append(y)
+    return vend_code_list
 
+# Generate a 10 character alphanumeric string to serve as a unique item number
 def gen_item_num():
     num_list = []
-    for i in range(1000):
+    for i in row_count:
         x = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
         num_list.append(x)
     return num_list
-    
+
+# Concatenate the alphanumerics with the vendor codes to create product numbers
 def gen_prod_num():
     prod_list = []
     vend_list = gen_vend_code()
@@ -26,7 +32,33 @@ def gen_prod_num():
         p = random.choice(vend_list) + n
         prod_list.append(p)
     return prod_list
+    
+# Generate a cost per unit between 0.01 and 99.99 for each product
+def gen_unit_cost():
+    cost_list = []
+    for item in row_count:
+        cost = round(random.uniform(0.01, 99.99), 2)
+        cost_list.append(cost)
+    return cost_list
 
+# Assign a random product category to each item
+def gen_item_cat():
+    prod_cat = ['Disposables','Utensils','China','Glassware','Plastics','Textiles','Equipment']
+    cat_list = []
+    for item in row_count:
+        category = random.choice(prod_cat)
+        cat_list.append(category)
+    return cat_list
+
+# Determine a random lead time between 1 and 31 days
+def gen_lead_time():
+    lead_times = [] 
+    for item in row_count:
+        lead = random.randint(1,31)
+        lead_times.append(lead)
+    return lead_times
+
+# Create the data frame and add additional columns
 np.random.seed(101)
 
 headers = ['PER1', 'PER2', 'PER3', 'PER4', 'PER5','PER6',\
@@ -34,16 +66,19 @@ headers = ['PER1', 'PER2', 'PER3', 'PER4', 'PER5','PER6',\
 
 df = pd.DataFrame(randn(1000,12), gen_prod_num(), headers).round(decimals=1) * 10 + 10
 df.index.name = 'ITEM'
+
 df['Vendor'] = df.index.astype(str).str.slice(stop=3)
-df['Total Use'] = df.sum(axis=1)
+df['12mo Use'] = df.sum(axis=1)
 df['Unit Cost'] = gen_unit_cost()
 df['Avg Sell'] = df['Unit Cost']*round(random.uniform(1,2),2) # Generate a random average sell price for each item
 df['Lead Time'] = gen_lead_time()
 df['Item Category'] = gen_item_cat()
 
+# Write the dataframe to csv
 today = date.today()
 file_date = today.strftime("%b-%d-%Y")
 
-file_path = r'your file path here\inv_data-' + file_date + '.csv'
+file_path = r'C:\Users\aidut\Documents\Inventory\inv_data-' + file_date + '.csv'
 
 df.to_csv(file_path)
+
